5c4ceca5b19a52257b14f76e94ca59ffc6b01277
Ayush Kumar <ayushk8295@gmail.com>
2025-01-10 15:44:06 +0530

    adding comment section full functionality

--- blog-web-app/client/src/components/PostComment.jsx
+++ blog-web-app/client/src/components/PostComment.jsx
@@ -7,8 +7,37 @@ function PostComment({ postId }) {
   // console.log(postId);
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
+  // console.log(comment);
   const handleSubmitComment = async (e) => {
     e.preventDefault();
+    try {
+      const res = await fetch(
+        `http://localhost:3000/server/comment/add-comment
+        `,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            content: comment,
+            userId: currentUser._id,
+            postId,
+          }),
+        }
+      );
+
+      const data = await res.json();
+      // console.log(data);
+      if (res.ok) {
+        setComment("");
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   return (
     <div className="max-w-2xl p-3 w-full mx-auto flex flex-col gap-3">
@@ -41,8 +70,10 @@ function PostComment({ postId }) {
           className="w-full border flex flex-col gap-3 border-teal-500 rounded-md p-3"
         >
           <Textarea
+            className="font-light"
             placeholder="Write your comment..."
             rows="3"
+            value={comment}
             onChange={(e) => setComment(e.target.value)}
           />
           <div className="flex justify-between items-center w-full">
