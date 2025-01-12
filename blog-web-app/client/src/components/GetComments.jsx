054d33f20e0c80ae75b87c1e8a53a7b89b3661f2
Ayush Kumar <ayushk8295@gmail.com>
2025-01-12 20:04:08 +0530

    add partial functionality to the edit comment

--- blog-web-app/client/src/components/GetComments.jsx
+++ blog-web-app/client/src/components/GetComments.jsx
@@ -1,11 +1,18 @@
-import { Button } from "flowbite-react";
+import { Button, Textarea } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 // import { BiLike } from "react-icons/bi";
 import { BiSolidLike } from "react-icons/bi";
 
-function GetComments({ comment, handleLike }) {
+function GetComments({
+  comment,
+  handleLikeComment,
+  handleDeleteComment,
+  handleEditedComment,
+}) {
   const [user, setUser] = useState("");
+  const [editComment, setEditComment] = useState(false);
+  const [editedContent, setEditedContent] = useState(comment.content);
   // console.log(comment);
   const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
@@ -25,6 +32,9 @@ function GetComments({ comment, handleLike }) {
       console.log(error.message);
     }
   }, [comment]);
+  const handleEditComment = () => {
+    setEditComment(true);
+  };
   return (
     <div className="border-b p-3 my-3 rounded-sm">
       <div className="flex gap-2">
@@ -33,27 +43,68 @@ function GetComments({ comment, handleLike }) {
           className="w-10 h-10 rounded-full"
           alt=""
         />
-        <div className=" flex flex-col gap-1">
+        <div className=" flex flex-col gap-1 w-full">
           <p className="text-xs font-extralight">@{user.username}</p>
-          <div className="font-light">{comment.content}</div>
-          <div className="flex font-extralight gap-2 mt-4 text-sm items-center">
-            <p onClick={() => handleLike(comment._id)}>
-              <BiSolidLike
-                className={`text-2xl hover:text-blue-600 cursor-pointer ${
-                  currentUser &&
-                  comment.likes.includes(currentUser._id) &&
-                  "text-blue-600"
-                }`}
+          {editComment ? (
+            <>
+              <Textarea
+                className="font-light w-full"
+                placeholder="Write your comment..."
+                rows="3"
+                value={editedContent}
+                onChange={(e) => setEditedContent(e.target.value)}
               />
-            </p>
-            {comment.likesCount !== 0 && <p>{comment.likesCount} Likes</p>}
-            {currentUser && currentUser._id === user._id && (
-              <>
-                <p>Edit</p>
-                <p>Delete</p>
-              </>
-            )}
-          </div>
+              <div className="flex justify-end gap-3">
+                <Button
+                  onClick={() => handleEditedComment(comment._id, editComment)}
+                  pill
+                >
+                  Submit
+                </Button>
+                <Button outline pill onClick={() => setEditComment(false)}>
+                  Cancel
+                </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="font-light">{comment.content}</div>
+              <div className="flex font-extralight gap-2 mt-4 text-sm items-center">
+                <button
+                  type="button"
+                  onClick={() => handleLikeComment(comment._id)}
+                >
+                  <BiSolidLike
+                    className={`text-2xl cursor-pointer ${
+                      currentUser &&
+                      comment.likes.includes(currentUser._id) &&
+                      "text-blue-600"
+                    }`}
+                  />
+                </button>
+                {comment.likesCount !== 0 && <p>{comment.likesCount} Likes</p>}
+                {currentUser &&
+                  (currentUser.isAdmin || currentUser._id === user._id) && (
+                    <>
+                      <button
+                        type="button"
+                        className="hover:underline hover:text-green-600"
+                        onClick={handleEditComment}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => handleDeleteComment(comment._id)}
+                        className="hover:underline hover:text-red-600"
+                      >
+                        Delete
+                      </button>
+                    </>
+                  )}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
