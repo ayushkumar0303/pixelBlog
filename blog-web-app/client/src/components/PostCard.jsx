ee0803df4a47be50e59b06a6c70fc6f36fee99ab
Ayush Kumar <ayushk8295@gmail.com>
2025-01-14 20:15:38 +0530

    add recent articles to the dashboard

---
+++ blog-web-app/client/src/components/PostCard.jsx
@@ -0,0 +1,32 @@
+import { Badge } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function PostCard({ post }) {
+  return (
+    <div className="group relative border p-2 rounded-lg flex flex-col flex-1 gap-1 group-hover:border-9 overflow-hidden">
+      <Link to={`/posts/${post.slug}`}>
+        <img
+          src={post.postImage}
+          alt=""
+          className="rounded-lg w-full h-[260px] object-cover group-hover:h-[200px] transition-all duration-300"
+        />
+      </Link>
+      <div className="flex flex-col items-start gap-1">
+        <Link to={`/posts/${post.slug}`}>
+          <h5>{post.title} </h5>
+        </Link>
+        <span className="text-sm italic">{post.category}</span>
+        <Link
+          to={`/posts/${post.slug}`}
+          className="z-10 absolute bottom-[-200px] group-hover:bottom-0 left-0 text-center right-0 group-hover:border-2 group-hover:border-teal-500 hover:bg-teal-500 p-1 m-3
+          rounded-lg transition-all duration-300"
+        >
+          Read more...
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default PostCard;
--- blog-web-app/client/src/components/PostComment.jsx
+++ blog-web-app/client/src/components/PostComment.jsx
@@ -1,14 +1,16 @@
-import { Button, Textarea } from "flowbite-react";
+import { Button, Card, Textarea } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import GetComments from "./GetComments";
+import PostCard from "./PostCard";
 
 function PostComment({ postId }) {
   // console.log(postId);
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState("");
+  const [postData, setPostData] = useState([]);
   const navigate = useNavigate();
   // console.log(comments);
 
@@ -25,6 +27,19 @@ function PostComment({ postId }) {
       };
 
       getComments();
+
+      const fetchRecentArticles = async () => {
+        const res = await fetch(
+          `http://localhost:3000/server/post/get-posts/?limit=3`
+        );
+        const data = await res.json();
+        console.log(data);
+        if (res.ok) {
+          setPostData(data.posts);
+        }
+      };
+
+      fetchRecentArticles();
     } catch (error) {
       console.log(error.message);
     }
@@ -125,71 +140,81 @@ function PostComment({ postId }) {
     );
   };
   return (
-    <div className="max-w-2xl p-3 w-full mx-auto flex flex-col gap-3">
-      {currentUser ? (
-        <div className="flex gap-2">
-          <p>Signed in as:</p>
-          <img
-            src={currentUser.profilePicture}
-            className="w-5 h-5 object-cover rounded-full"
-            alt=""
-          />
-          <Link
-            className="text-cyan-500 hover:underline"
-            to={`/dashboard/?tab=profile`}
-          >
-            @{currentUser.username}
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <p>you must signed in to comment</p>
-          <Link className="text-blue-500" to="/signin">
-            Sign in
-          </Link>
-        </div>
-      )}
-      {currentUser && (
-        <form
-          onSubmit={handleSubmitComment}
-          className="w-full border flex flex-col gap-3 border-teal-500 rounded-md p-3"
-        >
-          <Textarea
-            spellCheck="false"
-            className="font-light"
-            placeholder="Write your comment..."
-            rows="3"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-          />
-          <div className="flex justify-between items-center w-full">
-            <p>{200 - comment.length} characters remaining</p>
-            <Button type="submit">Submit</Button>
-          </div>
-        </form>
-      )}
-      {comments.length === 0 ? (
-        <h1>No comments yet!</h1>
-      ) : (
-        <div className="">
-          <p className="pb-3 my-2 border-b">
-            Comments{" "}
-            <span className="border border-gray-500 py-1 px-2 rounded-md">
-              {comments.length}
-            </span>
-          </p>
-          {comments.map((comment) => (
-            <GetComments
-              comment={comment}
-              key={comment._id}
-              handleLikeComment={handleLikeComment}
-              handleDeleteComment={handleDeleteComment}
-              handleEditedComment={handleEditedComment}
+    <>
+      <div className="max-w-2xl p-3 w-full mx-auto flex flex-col gap-3 ">
+        {currentUser ? (
+          <div className="flex gap-2">
+            <p>Signed in as:</p>
+            <img
+              src={currentUser.profilePicture}
+              className="w-5 h-5 object-cover rounded-full"
+              alt=""
             />
+            <Link
+              className="text-cyan-500 hover:underline"
+              to={`/dashboard/?tab=profile`}
+            >
+              @{currentUser.username}
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <p>you must signed in to comment</p>
+            <Link className="text-blue-500" to="/signin">
+              Sign in
+            </Link>
+          </div>
+        )}
+        {currentUser && (
+          <form
+            onSubmit={handleSubmitComment}
+            className="w-full border flex flex-col gap-3 border-teal-500 rounded-md p-3"
+          >
+            <Textarea
+              spellCheck="false"
+              className="font-light"
+              placeholder="Write your comment..."
+              rows="3"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <div className="flex justify-between items-center w-full">
+              <p>{200 - comment.length} characters remaining</p>
+              <Button type="submit">Submit</Button>
+            </div>
+          </form>
+        )}
+        {comments.length === 0 ? (
+          <h1>No comments yet!</h1>
+        ) : (
+          <div className="">
+            <p className="pb-3 my-2 border-b">
+              Comments{" "}
+              <span className="border border-gray-500 py-1 px-2 rounded-md">
+                {comments.length}
+              </span>
+            </p>
+            {comments.map((comment) => (
+              <GetComments
+                comment={comment}
+                key={comment._id}
+                handleLikeComment={handleLikeComment}
+                handleDeleteComment={handleDeleteComment}
+                handleEditedComment={handleEditedComment}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col items-center gap-3 max-w-4xl">
+        <h1 className="text-lg">Recent articles</h1>
+        <div className="flex gap-4 flex-wrap ">
+          {postData.map((post) => (
+            <PostCard post={post} key={post._id} />
           ))}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
 
